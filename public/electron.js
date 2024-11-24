86b84e064b9933d478d091a9a6ecf1bcb30f3d7e
dansad <dansad.dev@fastmail.com>
2024-11-23 16:07:02 -0800

    Prettier formatting

--- public/electron.js
+++ public/electron.js
@@ -1,45 +1,50 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const url = require('url');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
 
 function createWindow() {
-
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false, 
-      enableRemoteModule: true, 
-      webSecurity: true
+      contextIsolation: false,
+      enableRemoteModule: true,
+      webSecurity: true,
+    },
+  });
+  mainWindow.webContents.session.webRequest.onHeadersReceived(
+    (details, callback) => {
+      callback({
+        responseHeaders: {
+          ...details.responseHeaders,
+          "Content-Security-Policy": [
+            "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline';",
+          ],
+        },
+      });
     }
-  });
-  mainWindow.webContents.session.webRequest.onHeadersReceived((details, callback) => {
-    callback({
-      responseHeaders: {
-        ...details.responseHeaders,
-        'Content-Security-Policy': ["default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline';"]
-      }
+  );
+  const startUrl =
+    process.env.ELECTRON_START_URL ||
+    url.format({
+      pathname: path.join(__dirname, "../build/index.html"),
+      protocol: "file:",
+      slashes: true,
     });
-  });
-  const startUrl = process.env.ELECTRON_START_URL || url.format({
-    pathname: path.join(__dirname, '../build/index.html'),
-    protocol: 'file:',
-    slashes: true
-  });
-  console.log(`Loading URL: ${startUrl}`); 
+  console.log(`Loading URL: ${startUrl}`);
   mainWindow.loadURL(startUrl);
-  mainWindow.setMenu(null); 
+  mainWindow.setMenu(null);
 }
 
 app.whenReady().then(createWindow);
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => { 
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
